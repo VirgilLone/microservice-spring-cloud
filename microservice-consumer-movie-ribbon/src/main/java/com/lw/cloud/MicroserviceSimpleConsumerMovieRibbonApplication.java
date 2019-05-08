a6a88863c0f7ce543cb6363d427b9dede7380d12
luo.wen <wyluowen@163.com>
2019-05-08 17:37:50 +0800

    使用代码自定义配置ribbon

--- microservice-consumer-movie-ribbon/src/main/java/com/lw/cloud/MicroserviceSimpleConsumerMovieRibbonApplication.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/lw/cloud/MicroserviceSimpleConsumerMovieRibbonApplication.java
@@ -1,14 +1,19 @@
 package com.lw.cloud;
 
+import com.lw.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "microservice-provider-user",configuration = TestConfiguration.class)
 public class MicroserviceSimpleConsumerMovieRibbonApplication {
 
 	@Bean
---
+++ microservice-consumer-movie-ribbon/src/main/java/com/lw/config/TestConfiguration.java
@@ -0,0 +1,23 @@
+package com.lw.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: 自定义空值ribbon
+ * @author: luo.wen
+ * @createTime: 2019/5/8
+ */
+@Configuration //注解不能在@conponentScan扫的路径下
+//@RibbonClient(name = "microservice-provider-user",configuration = TestConfiguration.class)
+public class TestConfiguration {
+
+    @Bean
+    public IRule ribbonRule() {
+        return new RandomRule();
+    }
+
+}
