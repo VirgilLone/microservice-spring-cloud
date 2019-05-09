3ee578112a4588e29fe2df4c4074ebc89c2bb639
luo.wen <wyluowen@163.com>
2019-05-09 22:19:44 +0800

    添加feign调用的日志和级别

--- microservice-consumer-movie-feign-customizing/src/main/java/com/lw/config/MyConfiguration.java
+++ microservice-consumer-movie-feign-customizing/src/main/java/com/lw/config/MyConfiguration.java
@@ -1,13 +1,14 @@
 package com.lw.config;
 
 import feign.Contract;
+import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @description: 自定义的feign的配置
- * @author: luo.wen
- * @createTime: 2019/5/9
+ * @description:自定义的feign的配置
+ * @author:luo.wen
+ * @createTime:2019/5/9
  */
 @Configuration
 public class MyConfiguration {
@@ -15,6 +16,11 @@ public class MyConfiguration {
     @Bean
     public Contract feignContract(){
         return new feign.Contract.Default();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
     }
 
 }
