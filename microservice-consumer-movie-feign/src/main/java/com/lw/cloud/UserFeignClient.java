9ed1f5413e784cc5dee69167f35f07e3186dcff4
luo.wen <wyluowen@163.com>
2019-05-09 16:21:43 +0800

    1

--- microservice-consumer-movie-feign/src/main/java/com/lw/cloud/UserFeignClient.java
+++ microservice-consumer-movie-feign/src/main/java/com/lw/cloud/UserFeignClient.java
@@ -2,13 +2,12 @@ package com.lw.cloud;
 
 import com.lw.cloud.entity.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("microservice-provider-user")
+@FeignClient(name = "microservice-provider-user")
 public interface UserFeignClient {
 
     @RequestMapping(value = "/simple/{id}",method = RequestMethod.GET)
