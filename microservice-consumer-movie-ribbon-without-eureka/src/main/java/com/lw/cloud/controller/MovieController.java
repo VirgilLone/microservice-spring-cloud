a830a5cbb4d90103e50ec926c33250bc672f4613
luo.wen <wyluowen@163.com>
2019-05-09 10:56:49 +0800

    不使用eureka的ribbon配置

---
+++ microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/lw/cloud/controller/MovieController.java
@@ -0,0 +1,37 @@
+package com.lw.cloud.controller;
+
+import com.lw.cloud.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by WYluo on 2018/4/7.
+ */
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+
+    @GetMapping("/movie/{id}")
+    public User findById(@PathVariable Long id){
+
+        //测试通过ribbon负载到哪个节点
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        System.out.println("-------->" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+
+        // http://localhost:7900/simple/
+        return this.restTemplate.getForObject("http://microservice-provider-user/simple/"+id,User.class);
+    }
+
+}
