ce2c32246b198271a663afcbdb1fff0833cdfee4
luo.wen <wyluowen@163.com>
2019-05-09 17:01:08 +0800

    自定义feign注解的调用

---
+++ microservice-consumer-movie-feign-customizing/src/main/java/com/lw/cloud/UserFeignClient.java
@@ -0,0 +1,17 @@
+package com.lw.cloud;
+
+import com.lw.config.MyConfiguration;
+import com.lw.cloud.entity.User;
+import feign.Param;
+import feign.RequestLine;
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+@FeignClient(name = "microservice-provider-user" , configuration = MyConfiguration.class)
+public interface UserFeignClient {
+
+    @RequestLine("GET /simple/{id}")
+    User findByIddd(@Param("id") Long id); // *feign在调用的时候两个坑：GetMapping组合注解不支持；@PathVariable要设置value
+
+
+
+}
