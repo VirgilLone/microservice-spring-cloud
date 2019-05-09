6f01361785f4e29cbdbd3010a9d8cd62bb8aa130
luo.wen <wyluowen@163.com>
2019-05-09 14:32:11 +0800

    使用feign接口进行远程调用

--- microservice-consumer-movie-feign/src/main/java/com/lw/cloud/controller/MovieController.java
+++ microservice-consumer-movie-feign/src/main/java/com/lw/cloud/controller/MovieController.java
@@ -1,10 +1,14 @@
 package com.lw.cloud.controller;
 
+import com.lw.cloud.UserFeignClient;
 import com.lw.cloud.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,10 +18,12 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MovieController {
 
+    @Autowired
+    private UserFeignClient userFeignClient;
 
-    @GetMapping("/movie/{id}")
-    public User findById(@PathVariable Long id){
-        return null;
+    @GetMapping( "/movie/{id}")
+    public User findByIdd(@PathVariable Long id){
+        return userFeignClient.findById(id);
     }
 
 }
